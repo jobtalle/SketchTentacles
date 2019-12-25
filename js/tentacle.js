faa77b1cb4daffe688c59e371a03592e1db36168
Job <jobtalle@hotmail.com>
2019-12-25 18:27:42 +0100

    Created a jellyfish

--- js/tentacle.js
+++ js/tentacle.js
@@ -1,36 +1,56 @@
-const Tentacle = function() {
+const Tentacle = function(length, onset, direction) {
     const noise = cubicNoiseConfig(Math.random());
+    const points = new Array(length << 2);
     let shift = 0;
 
-    this.update = timeStep => {
-        shift += timeStep * 0.7;
-    };
-
-    this.draw = context => {
-        let angle = 0;
+    const updatePoints = () => {
         let x = 0;
         let y = 0;
 
+        for (let i = 0; i < length; ++i) {
+            const index = i << 2;
+            const factor = i / (length - 1);
+            const angle = (cubicNoiseSample1(noise, i * 0.2 + 10000 - shift) - 0.5) * 8 * factor +
+                Math.pow(1 - factor, 5) * direction;
+            const dx = Math.cos(angle);
+            const dy = Math.sin(angle);
+            const d = 16;
+
+            x += dx * d;
+            y += dy * d;
+
+            points[index] = x;
+            points[index + 1] = y;
+            points[index + 2] = -dy;
+            points[index + 3] = dx;
+        }
+    };
+
+    this.update = timeStep => {
+        shift += timeStep * 0.8;
+
+        updatePoints();
+    };
+
+    this.draw = context => {
         context.beginPath();
-        context.moveTo(0, 0);
 
-        const l = 30;
-        const s = 0.2;
+        if (onset === 0)
+            context.moveTo(0, 0);
+        else
+            context.moveTo(points[(onset - 1) << 2], points[((onset - 1) << 2) + 1]);
 
-        for (let i = 0; i < l; ++i) {
-            const f = Math.pow(i / l, 1);
+        for (let i = onset; i < length; ++i) {
+            const index = i << 2;
 
-            angle += (cubicNoiseSample1(noise, i * s + shift) - 0.5) * 5 * f;
-
-            const d = 12;
-
-            x += Math.cos(angle) * d;
-            y += Math.sin(angle) * d;
-
-            context.lineTo(x, y);
+            context.lineTo(
+                points[index],
+                points[index + 1]);
         }
 
-        context.strokeStyle = "blue";
+        context.strokeStyle = Tentacle.COLOR;
         context.stroke();
     };
 };
+
+Tentacle.COLOR = "white";
--- js/tentacles.js
+++ js/tentacles.js
@@ -1,16 +1,26 @@
 const Tentacles = function() {
-    const tentacle = new Tentacle();
+    const tentacles = new Array(Tentacles.ARMS);
 
     this.update = timeStep => {
-        tentacle.update(timeStep);
+        for (const tentacle of tentacles)
+            tentacle.update(timeStep);
     };
 
     this.draw = context => {
         context.save();
-        context.translate(300, 300);
+        context.translate(300, 450);
 
-        tentacle.draw(context);
+        for (const tentacle of tentacles)
+            tentacle.draw(context);
 
         context.restore();
     };
+
+    for (let i = 0; i < tentacles.length; ++i)
+        tentacles[i] = new Tentacle(
+            50,
+            i === 0 || i === tentacles.length - 1 ? 0 : 6,
+            Math.PI * (-0.5 + (i / (tentacles.length - 1))));
 };
+
+Tentacles.ARMS = 7;
